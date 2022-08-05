5540d99a53a819438d07d74f14f657d5e0d565e6
Ahirsch-bit <avi.matnasoreq@gmail.com>
2022-08-05 10:54:49 +0300

    added 7 boom

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from lesson_1.lesson1 import exercise_1, exercise_4, exercise_6
 from lesson_1.lesson1_bonus import question1, question2, question3, question4, question5
 
-exercise_1()
-exercise_4()
-exercise_6()
+# exercise_1()
+# exercise_4()
+# exercise_6()
+#
+# a = int(input("Enter value for 'a'"))
+# b = int(input("Enter value for 'b'"))
+#
+# question1(a, b)
+# question2(a, b)
+# question3(a, b)
+# question4(a, b)
+# question5(a, b)
+from lesson_2.seven_boom import seven_boom
 
-a = int(input("Enter value for 'a'"))
-b = int(input("Enter value for 'b'"))
-
-question1(a, b)
-question2(a, b)
-question3(a, b)
-question4(a, b)
-question5(a, b)
+seven_boom()
